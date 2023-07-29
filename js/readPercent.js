6a64c4f71fe3ce2eec344e634522c584da520cfd
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2023-07-29 12:48:27 +0800

    Site updated: 2023-07-29 12:48:26

---
+++ js/readPercent.js
@@ -0,0 +1,57 @@
+window.onscroll = percent;// ִ�к���
+// ҳ��ٷֱ�
+function percent() {
+    let a = document.documentElement.scrollTop || window.pageYOffset, // ��ȥ�߶�
+        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // ������ҳ�߶�
+        result = Math.round(a / b * 100), // ����ٷֱ�
+        up = document.querySelector("#go-up") // ��ȡ��ť
+
+    if (result <= 95) {
+        up.childNodes[0].style.display = 'none'
+        up.childNodes[1].style.display = 'block'
+        up.childNodes[1].innerHTML = result;
+    } else {
+        up.childNodes[1].style.display = 'none'
+        up.childNodes[0].style.display = 'block'
+    }
+}
+
+
+var leonus = {
+    linkCom: e => {
+        var t = document.querySelector(".el-textarea__inner");
+        "bf" == e ? (t.value = "```yml\n", t.value += "- name: \n  link: \n  avatar: \n  descr: \n  siteshot: ", t.value += "\n```", t.setSelectionRange(15, 15)) : (t.value = "վ�����ƣ�\nվ���ַ��\nͷ�����ӣ�\nվ��������\nվ���ͼ��", t.setSelectionRange(5, 5)), t.focus()
+    },
+    owoBig: () => {
+        if (!document.getElementById("post-comment") || document.body.clientWidth < 768) return;
+        let e = 1,
+            t = "",
+            o = document.createElement("div"),
+            n = document.querySelector("body");
+        o.id = "owo-big", n.appendChild(o), new MutationObserver((l => {
+            for (let a = 0; a < l.length; a++) {
+                let i = l[a].addedNodes,
+                    s = "";
+                if (2 == i.length && "OwO-body" == i[1].className) s = i[1];
+                else {
+                    if (1 != i.length || "tk-comment" != i[0].className) continue;
+                    s = i[0]
+                }
+                s.onmouseover = l => {
+                    e && ("OwO-body" == s.className && "IMG" == l.target.tagName || "tk-owo-emotion" == l.target.className) && (e = 0, t = setTimeout((() => {
+                        let e = 3 * l.path[0].clientHeight,
+                            t = 3 * l.path[0].clientWidth,
+                            a = l.x - l.offsetX - (t - l.path[0].clientWidth) / 2,
+                            i = l.y - l.offsetY;
+                        a + t > n.clientWidth && (a -= a + t - n.clientWidth + 10), a < 0 && (a = 10), o.style.cssText = `display:flex; height:${e}px; width:${t}px; left:${a}px; top:${i}px;`, o.innerHTML = `<img src="${l.target.src}">`
+                    }), 300))
+                }, s.onmouseout = () => {
+                    o.style.display = "none", e = 1, clearTimeout(t)
+                }
+            }
+        })).observe(document.getElementById("post-comment"), {
+            subtree: !0,
+            childList: !0
+        })
+    },
+};
